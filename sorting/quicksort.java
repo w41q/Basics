88b0a5f64c356c0784a060ce22ce0aa7c0a1b2f7
Qiang Wang <alexwang041@gmail.com>
2015-08-10 17:28:49 -0700

    fix bug, add another version

--- sorting/quicksort.java
+++ sorting/quicksort.java
@@ -3,7 +3,8 @@ public class quicksort
 	public static void main(String[] args)
 	{
 		int[] a = {4,1,6,9,0,3,5,8,2,7};
-		qs(a, 0, a.length-1);
+		//int[] a = {-1,2,0,2};
+		qs2(a, 0, a.length-1);
 		for(int i : a)
 			System.out.print(i+" ");
 	}
@@ -19,24 +20,58 @@ public class quicksort
 	
 	public static int partition(int[] a, int begin, int end)
 	{
+		int pivot = a[begin];
+		int idx = begin, next = begin;
+		while(idx<=end)
+		{
+			if(a[idx]<pivot)
+			{
+				int tmp = a[idx];
+				a[idx] = a[next];
+				a[next] = tmp;
+				next++;
+			}
+			idx++;
+		}
+		if(a[next]!=pivot)
+		{
+			for(int i=next;i<=end;i++)
+			{
+				if(a[i]==pivot)
+				{
+					int tmp = a[i];
+					a[i] = a[next];
+					a[next] = tmp;
+				}
+			}
+		}
+		return next;
+	}
+	
+	public static void qs2(int[] a, int begin, int end)
+	{
+		if(begin>=end)
+			return;
 		int pivot = a[(begin+end)/2];
-		System.out.println(pivot);
 		int left = begin, right = end;
-		while(left<right)
+		while(left<=right)
 		{
 			while(a[left]<pivot)
 				left++;
 			while(a[right]>pivot)
 				right--;
-			if(left<right)
+			if(left<=right)	// = is to prevent infinite loop when left=right && num[left]==pivot
 			{
 				int tmp = a[left];
 				a[left] = a[right];
 				a[right] = tmp;
+				left++;
+				right--;
 			}
 		}
-		if(left!=right)
-			System.out.println(left+" "+right);
-		return right;
+		if(begin<right)
+			qs2(a, begin, right);
+		if(left<end)
+			qs2(a, left, end);
 	}
 }
