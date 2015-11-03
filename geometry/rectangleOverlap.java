ef0abc8df0b0a741b979f1057f4304c03570edf3
Qiang Wang <alexwang041@gmail.com>
2015-11-02 22:18:24 -0800

    Create rectangleOverlap.java

---
+++ geometry/rectangleOverlap.java
@@ -0,0 +1,28 @@
+class rectangleOverlap
+{
+	public static void main(String[] args)
+	{
+		Point l1 = new Point(0, 10), r1 = new Point(10, 0);
+		Point l2 = new Point(5, 5), r2 = new Point(15, 0);
+		System.out.println(overlap(l1, r1, l2, r2));
+	}
+	
+	public static boolean overlap(Point l1, Point r1, Point l2, Point r2)
+	{
+		if(l1.x>r2.x || l2.x>r1.x)
+			return false;
+		if(l1.y<r2.y || l2.y<r1.y)
+			return false;
+		return true;
+	}
+}
+
+class Point
+{
+	public double x, y;
+	public Point(double x, double y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+}
